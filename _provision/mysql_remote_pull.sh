79a54953dbafa5400c6069263500826eca046b5e
Daniel <contact@danielupshaw.com>
2015-08-26 14:17:23 -0400

    Updates

--- _provision/mysql_remote_pull.sh
+++ _provision/mysql_remote_pull.sh
@@ -9,13 +9,15 @@ remote_database_name="${6}"
 remote_database_user="${7}"
 remote_database_pass="${8}"
 
-echo
-echo ">>> Importing remote database"
-echo
-echo "Enter SSH password, followed by remote database password (if prompted)"
-echo
-if [[ -n ${remote_database_pass} ]]; then
-	ssh "${remote_database_ssh_user}@${remote_database_ssh_host}" mysqldump --user="${remote_database_user}" --password="\"${remote_database_pass}\"" "${remote_database_name}" | mysql -u"${database_user}" -p"${database_pass}" "${database_name}"
-else
-	ssh "${remote_database_ssh_user}@${remote_database_ssh_host}" mysqldump --user="${remote_database_user}" -p "${remote_database_name}" | mysql -u"${database_user}" -p"${database_pass}" "${database_name}"
+if [ -t 1 ] ; then
+	echo
+	echo ">>> Importing remote database"
+	echo
+	echo "Enter SSH password, followed by remote database password (if prompted)"
+	echo
+	if [[ -n ${remote_database_pass} ]]; then
+		ssh "${remote_database_ssh_user}@${remote_database_ssh_host}" mysqldump --user="${remote_database_user}" --password="\"${remote_database_pass}\"" "${remote_database_name}" | mysql -u"${database_user}" -p"${database_pass}" "${database_name}"
+	else
+		ssh "${remote_database_ssh_user}@${remote_database_ssh_host}" mysqldump --user="${remote_database_user}" -p "${remote_database_name}" | mysql -u"${database_user}" -p"${database_pass}" "${database_name}"
+	fi
 fi
