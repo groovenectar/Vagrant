27e945cc1c8b7306be066d538ace38ad7252a0df
Daniel <contact@danielupshaw.com>
2015-08-31 10:23:38 -0400

    xdebug max

--- _provision/php.sh
+++ _provision/php.sh
@@ -71,6 +71,7 @@ xdebug.show_local_vars=1
 xdebug.var_display_max_depth = 5
 xdebug.var_display_max_children = 256
 xdebug.var_display_max_data = 1024
+xdebug.max_nesting_level = 350
 EOF
 
 	# PHP Error Reporting Config
